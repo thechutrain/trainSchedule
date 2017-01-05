ad34bfc4bf5a5c60d2f269cd376def045db4a35e
thechutrain <chu.chuyalan@gmail.com>
2017-01-05 18:21:27 -0500

    basic version 2 of the basic train exercise thing

---
+++ assets/javascript/main_v2.js
@@ -0,0 +1,34 @@
+function getFormData(){
+  var trainName = $("#trainName").val().trim();
+  var destination = $("#destination").val().trim();
+  var firstTrain = $("#firstTrain").val().trim();
+  var frequency = $("#frequency").val().trim();
+  var trainData = {
+    0: trainName,
+    1: destination,
+    2: firstTrain,
+    3: frequency,
+  };
+  return trainData;
+}
+
+function clearFormData(){
+  var trainName = $("#trainName").val("");
+  var origin = $("#origin").val("");
+  var destination = $("#destination").val("");
+  var frequency = $("#frequency").val("");
+}
+
+// ---------------------------------------------------
+// Handle submit button
+$("#submit").on("click", function(e){
+  //1) prevent default
+  e.preventDefault();
+  // 2) get the form data
+  var data = getFormData();
+  // 3) submit the train data
+  addTrain(data[0], data[1], data[2], data[3]);
+  // console.info("Entered Data");
+  //4) clear form
+  clearFormData();
+})
