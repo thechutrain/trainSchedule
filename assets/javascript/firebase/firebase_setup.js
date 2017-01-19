ef6db66f56b90392ee65e864c79d5d3a94eb0fd2
thechutrain <chu.chuyalan@gmail.com>
2017-01-19 15:54:51 -0500

    added my firebase set up

---
+++ assets/javascript/firebase/firebase_setup.js
@@ -0,0 +1,22 @@
+// --------------------------------------------------------------
+// Initialize Firebase -- chu.chuyalan@gmail.com firebase
+// var config = {
+//     apiKey: "AIzaSyB9T1l0-t_hJVxrRcSoOP57wpwbsPIsq44",
+//     authDomain: "traintracker-3dfed.firebaseapp.com",
+//     databaseURL: "https://traintracker-3dfed.firebaseio.com",
+//     storageBucket: "traintracker-3dfed.appspot.com",
+//     messagingSenderId: "395302672649"
+//   };
+// firebase.initializeApp(config);
+
+// --------------------------------------------------------------
+// gargantuanFM account
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBGUCbccKjDUbh2ggonO3ssFe5C4kbGQV0",
+    authDomain: "trainscheduler-32fe6.firebaseapp.com",
+    databaseURL: "https://trainscheduler-32fe6.firebaseio.com",
+    storageBucket: "trainscheduler-32fe6.appspot.com",
+    messagingSenderId: "1918997772"
+  };
+  firebase.initializeApp(config);
